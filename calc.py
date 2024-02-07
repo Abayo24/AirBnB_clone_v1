f51bad0a45f3ea3ccfe1aaf5d8559d3aa306c6ba
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-02-08 00:06:40 +0300

    pycodestyle compliant

---
+++ calc.py
@@ -0,0 +1,17 @@
+class Calc:
+    """calculator that adds subtracts, multiplies and divides only"""
+    def add(a, b):
+        """adds two numbers"""
+        return a + b
+
+    def subtract(a, b):
+        """subtracts b from a"""
+        return a - b
+
+    def multiply(a, b):
+        """multiplies two numbers"""
+        return a * b
+
+    def divide(a, b):
+        """divides a by b"""
+        return a / b
