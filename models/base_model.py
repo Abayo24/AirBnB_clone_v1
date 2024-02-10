327204fb2c9238709d058f95b4c38cf26d944124
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-02-10 13:44:35 +0300

    pycodestyle correction

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel:
                     setattr(self, key, datetime.strptime(value, time_format))
                 else:
                     setattr(self, key, value)
-        else:    
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -39,6 +39,7 @@ class BaseModel:
         base_dict['updated_at'] = self.updated_at.isoformat()
         return base_dict
 
+
 if __name__ == "__main__":
     my_model = BaseModel()
     my_model.name = "My First Model"
@@ -50,7 +51,8 @@ if __name__ == "__main__":
     print(my_model_json)
     print("JSON of my_model:")
     for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+        print("\t{}: ({}) - {}"
+              .format(key, type(my_model_json[key]), my_model_json[key]))
 
     print("--")
     my_new_model = BaseModel(**my_model_json)
