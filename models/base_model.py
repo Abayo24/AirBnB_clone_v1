d06d60d362e9fb9cea2fd45f1dde8eb767a6bcec
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-02-13 18:25:00 +0300

    tests

--- models/base_model.py
+++ models/base_model.py
@@ -19,15 +19,15 @@ class BaseModel:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
         models.storage.new(self)
 
     def save(self):
         """updates the public instance attribute updated_at \
                 with the current datetime"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
