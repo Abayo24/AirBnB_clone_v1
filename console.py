dc5f104019804ab748ea7e3884b9fb689c0dac6d
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-02-18 10:09:01 +0300

    documentation

--- console.py
+++ console.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""""""
+"""contains the entry point of the command interpreter"""
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """"""
+    """class HBNBCommand"""
     prompt = "(hbnb)"
 
     def do_quit(self, arg):
