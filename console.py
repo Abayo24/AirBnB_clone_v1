c85fa5891d77916fada49bc19c2621bdc179fd7b
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-02-18 10:31:25 +0300

    prompt

--- console.py
+++ console.py
@@ -6,7 +6,7 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """Command interpreter for HBNB project"""
 
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
 
     def do_quit(self, arg):
         """Quit command to exit the program
