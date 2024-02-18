d929632f792b3a4b0abfc664af50c7db8dc893f6
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-02-18 08:58:05 +0300

    testSave

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -15,7 +15,8 @@ class TestBaseModel(unittest.TestCase):
     def test_save(self):
         my_model = BaseModel()
         initial_updated_at = my_model.updated_at
-        current_updated_at = my_model.save()
+        my_model.save()
+        current_updated_at = my_model.updated_at
 
         self.assertNotEqual(initial_updated_at, current_updated_at)
 
